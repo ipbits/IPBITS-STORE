12dca506c11ee36ac8440d6910b8efefe74ed570
A <A@PP.AP>
2026-08-26 00:30:05 +0300

    update website

--- src/app/page.js
+++ src/app/page.js
@@ -52,7 +52,7 @@ const PAYMENT_ACCOUNTS = {
   FastPay: { title: 'FastPay Wallet', number: '07504060378', note: 'ژمارا وەسڵێ پشتی پارەدانێ بنڤیسە' },
   ZainCash: { title: 'Zain Cash', number: '07800000000', note: 'ژمارا وەسڵێ د فۆرمێ دا بنڤیسە' },
   PayPal: { title: 'PayPal', number: 'peshwarfarhad@gmail.com', note: 'پارەی ب شێوەیێ Friends & Family بنێرە و ناڤێ خۆ بنڤیسە' },
-  USDT: { title: 'USDT (TRC20)', number: 'TYu8VnXXXXXXXXXXXXXXXXXXXXX', note: 'تەنێ ل سەر تۆڕا Tron (TRC20) فرێکە و TxID بنڤیسە' }
+  USDT: { title: 'USDT (TRC20)', number: 'TUeqkjzFdD7b1EtnAJL9tbzB1uN8wDbU6T', note: 'تەنێ ل سەر تۆڕا Tron (TRC20) فرێکە و TxID بنڤیسە' }
 };
 
 export default function StorePage() {
